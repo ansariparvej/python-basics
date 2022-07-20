e7284db3cd64b6d74798adb6044751247c6ba42e
ansariparvej <ansarip82@gmail.com>
2022-07-20 22:20:25 +0530

    testing upload from pycharm.

---
+++ PQR/test library.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import scrapy
+from bs4 import BeautifulSoup
+import seaborn
+import torch
+import keras
+torch.cuda.is_available
+print("Required Library is already Installed.")
